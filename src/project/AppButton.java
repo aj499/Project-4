562f81b6dd6dbf0e3ff5e1a4573714599457fd09
Adam Jacobson <aj499@georgetown.edu>
2014-04-19 13:53:04 -0400

    Eclipse project init
    Created Eclipse project with project and test packages and the six
    files listed in the UML

---
+++ src/project/AppButton.java
@@ -0,0 +1,7 @@
+package project;
+
+import javax.swing.JButton;
+
+public class AppButton extends JButton{
+
+}
---
+++ src/project/WelcomeWindow.java
@@ -0,0 +1,10 @@
+package project;
+
+import javax.swing.JApplet;
+
+public class WelcomeWindow extends JApplet{
+	
+	public static void main(String[] args){
+		//do the thing
+	}
+}
