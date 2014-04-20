17497f0d2ca2e02d2a5401350ce70905bf1df54b
minlee2012 <minlee2012@users.noreply.github.com>
2014-04-20 15:12:53 -0400

    Update AppButton.java

--- src/project/AppButton.java
+++ src/project/AppButton.java
@@ -1,7 +1,47 @@
+/**
+ * AppButton
+ * @author Min Lee
+ * @version 1.0
+ * 
+ * The AppButton class implements a JButton that
+ * allows the program to get the IDs of certain 
+ * countries.
+ * 
+ */
 package project;
-
 import javax.swing.JButton;
 
 public class AppButton extends JButton{
 
-}
+	private static final long serialVersionUID = 1L;
+	private String id;
+	
+	/**
+	 * setId(String newId)
+	 * 
+	 * setter method for private data member id
+	 * 
+	 * @param newId
+	 * @return void
+	 */
+	public void setId(String newId){
+		
+		newId = id;
+		
+	}//setId
+	
+	/**
+	 * getId()
+	 * 
+	 * getter method for private data member id
+	 * 
+	 * @param void
+	 * @return void
+	 */
+	public String getId(){
+		
+		return id;
+		
+	}//getId
+	
+}//class AppButton
