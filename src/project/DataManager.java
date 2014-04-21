a8223c4e12c69f1adb40c36a0f7b0f2758b2180c
Adam Jacobson <aj499@georgetown.edu>
2014-04-21 18:04:21 -0400

    Begin work on MapPanel
    Misc work on MapPanel.
    One simple function implemented in DataManager to provide necessary
    functionality.

--- src/project/DataManager.java
+++ src/project/DataManager.java
@@ -1,5 +1,17 @@
 package project;
 
-public class DataManager {
+import java.util.HashMap;
+import java.util.Set;
 
+public class DataManager {
+	private HashMap<String, CountryData> countryData;
+	
+	/**
+	 * Returns a list of all the countries in the countryData hash.
+	 * 
+	 * @return a list of all the countries in the countryData hash
+	 */
+	public Set<String> getCountryList(){
+		return countryData.keySet();
+	}
 }
