493f3728d242b7c40744f509a0f2f45856de9da1
mtmcaneny <michaelmcaneny@Michaels-MacBook-Pro.local>
2014-04-21 22:47:38 -0400

    some data parsing for datamanager

--- src/project/DataManager.java
+++ src/project/DataManager.java
@@ -4,7 +4,48 @@ import java.util.HashMap;
 
 public class DataManager {
 	private HashMap<String, CountryData> countryData;
+	private HashMap<String, ContinentData> continentData;
+	private boolean dataLoaded;
+	private String fileLocation;
 	
+	
+	/**
+	 * Constructor takes a String that is the filepath
+	 * @param newFileLocation
+	 */
+	DataManager(String newFileLocation){
+		countryData = new HashMap<String, CountryData>();
+		continentData = new HashMap<String, ContinentData>();
+		
+		dataLoaded=false;
+		setFileLocation(newFileLocation);
+	}
+	
+	/**
+	 * sets the filepath
+	 * @param newFileLocation
+	 */
+	public void setFileLocation(String newFileLocation){
+		fileLocation = newFileLocation;
+	}
+	
+	/**
+	 * 
+	 */
+	
+	public void parseData(){
+		String countryName;
+		String gpdPerCapita, gdpRealGrowthRate, 
+		agriculturePercentageOfGdp, economicFreedomScore, lowestTenIncome,
+		highestTenIncome, majorIndustries, unemploymentRate, majorEconomicIssue,
+		makeADifferenceEconomic, photoPathEconomic;
+		
+		String lifeExpectancy, maternalMortalityRate, infantMortalityRate,
+		childrenUnderweightPercentage, physicianDensity, riskOfInfectiousDisease,
+		mostCommonDiseases, majorHealthIssue, makeADifferenceHealth, photoPathHealth;
+		
+		
+	}
 	/**
 	 * Returns a list of all the countries in the countryData hash.
 	 * 
