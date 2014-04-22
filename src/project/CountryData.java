0784b2dbb0fa582154f490bb38a256840b63ed79
joeblack24 <jvw8@georgetown.edu>
2014-04-21 22:22:36 -0400

    CountryData
    Added all strings for country data

--- src/project/CountryData.java
+++ src/project/CountryData.java
@@ -2,4 +2,27 @@ package project;
 
 public class CountryData {
 
+	private String countryName;
+	private String gpdPerCapita;
+	private String gdpRealGrowthRate; 
+	private String agriculturePercentageOfGdp;
+	private String economicFreedomScore;
+	private String lowestTenIncome;
+	private String highestTenIncome;
+	private String majorIndustries;
+	private String unemploymentRate;
+	private String majorEconomicIssue
+	private String makeADifferenceEconomic;
+	private String photoPathEconomic;
+	private String lifeExpectancy;
+	private String maternalMortalityRate;
+	private String infantMortalityRate;
+	private String childrenUnderweightPercentage;
+	private String physicianDensity;
+	private String riskOfInfectiousDisease;
+	private String mostCommonDiseases; 
+	private String majorHealthIssue;
+	private String makeADifferenceHealth;
+	private String photoPathHealth;
+	
 }
