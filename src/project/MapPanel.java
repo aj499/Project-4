a6e23207359c56bec69a45210c68c541b1ded655
Adam Jacobson <aj499@georgetown.edu>
2014-04-21 22:32:12 -0400

    New TODO
    just committing so I can merge; sorry for the bother

--- src/project/MapPanel.java
+++ src/project/MapPanel.java
@@ -186,6 +186,7 @@ public class MapPanel extends JPanel implements ActionListener, MouseListener{
 			Vector<String> subjectCountries = worldData.getDataForContinent(currentView).getCountryList();
 			
 			//TODO: do the actual quiz here
+			//TODO: should this be programmatic or pre-defined?!
 			//present question
 			//check answer
 			//profit/repeat!
