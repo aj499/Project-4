2bee000ff8269132b3c4c5b89aaf552d7f23940a
Adam Jacobson <aj499@georgetown.edu>
2014-04-29 00:32:21 -0400

    Delete ContinentTestData from package project

--- src/project/MapPanel.java
+++ src/project/MapPanel.java
@@ -463,7 +463,7 @@ public class MapPanel extends JPanel implements ActionListener, MouseListener{
 						setUpNextQuestion();
 					} catch (IOException e1) {
 						// TODO Auto-generated catch block
-						e1.printStackTrace();
+						e1.printStackTrace();//see what happened
 					}
 				}
 			}
